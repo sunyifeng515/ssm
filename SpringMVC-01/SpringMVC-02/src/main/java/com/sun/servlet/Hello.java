76655b9452c0d8b9c1a4cb5788be697c73e67175
sunyifeng <1010913943@qq.com>
2020-11-28 00:31:00 +0800

    1.0

---
+++ SpringMVC-01/SpringMVC-02/src/main/java/com/sun/servlet/Hello.java
@@ -0,0 +1,26 @@
+package com.sun.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Hello extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String method = req.getParameter("method");
+        if (method.equals("add")){
+            req.getSession().setAttribute("msg","执行了add方法");
+        }
+        if (method.equals("delete")){
+            req.getSession().setAttribute("msg","执行了delete方法");
+        }
+        req.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+}
---
+++ SpringMVC-01/SpringMVC-02/web/index.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: SunYiFeng
+  Date: 2020/11/27
+  Time: 23:51
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+<form action="/hello" method="post" ></form>
+  <input type="text" name="method">
+  <input type="submit">
+  </body>
+</html>
