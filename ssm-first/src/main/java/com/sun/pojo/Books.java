e7627e3a8b263357ea1de1245255b409167a5e9d
sunyifeng <1010913943@qq.com>
2020-11-29 22:48:44 +0800

    1.0

---
+++ ssm-first/src/main/java/com/sun/pojo/Books.java
@@ -0,0 +1,15 @@
+package com.sun.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Books {
+    private Integer bookID;
+    private String bookName;
+    private Integer bookCounts;
+    private String detail;
+}
---
+++ ssm-first/src/main/java/com/sun/service/BookServiceimpl.java
@@ -0,0 +1,36 @@
+package com.sun.service;
+
+import com.sun.dao.BookMapper;
+import com.sun.pojo.Books;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+public class BookServiceimpl implements BookService{
+
+    private BookMapper bookMapper;
+
+    public void setBookMapper(BookMapper bookMapper) {
+        this.bookMapper = bookMapper;
+    }
+
+    public int addBook(Books book) {
+        return bookMapper.addBook(book);
+    }
+
+    public int deleteBookById(int id) {
+        return bookMapper.deleteBookById(id);
+    }
+
+    public int updateBook(Books books) {
+        return bookMapper.updateBook(books);
+    }
+
+    public Books queryBookById(int id) {
+        return bookMapper.queryBookById(id);
+    }
+
+    public List<Books> queryAllBook() {
+        return bookMapper.queryAllBook();
+    }
+}
