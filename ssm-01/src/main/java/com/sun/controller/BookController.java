65d6a424435b0bad7cf5595bb2e04305f642b330
sunyifeng <1010913943@qq.com>
2020-12-05 18:44:41 +0800

    final

---
+++ ssm-01/src/main/java/com/sun/controller/BookController.java
@@ -0,0 +1,59 @@
+package com.sun.controller;
+
+import com.sun.pojo.Books;
+import com.sun.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/Book")
+public class BookController {
+    @Autowired
+    @Qualifier("BookServiceImpl")
+    private BookService bookService;
+
+    @RequestMapping("/allBook")
+    public String list(Model model){
+        List<Books> books = bookService.queryAllBook();
+        model.addAttribute("list",books);
+        return "allBook";
+    }
+    @RequestMapping("/toAddBook")
+    public String toAddPaper() {
+        return "addBook";
+    }
+
+    @RequestMapping("/addBook")
+    public String addPaper(Books books) {
+        System.out.println(books);
+        bookService.addBook(books);
+        return "redirect:/Book/allBook";
+    }
+    @RequestMapping("/toUpdateBook")
+    public String toUpdateBook(Model model, int id) {
+        Books books = bookService.queryBookById(id);
+        System.out.println(books);
+        model.addAttribute("book",books );
+        return "update";
+    }
+
+    @RequestMapping("/updateBook")
+    public String updateBook(Model model, Books book) {
+        System.out.println(book);
+        bookService.updateBook(book);
+        Books books = bookService.queryBookById(book.getBookID());
+        model.addAttribute("books", books);
+        return "redirect:/Book/allBook";
+    }
+    @RequestMapping("/del/{bookId}")
+    public String deleteBook(@PathVariable("bookId") int id) {
+        bookService.deleteBookById(id);
+        return "redirect:/Book/allBook";
+    }
+}
---
+++ ssm-01/src/test/java/myTest.java
@@ -0,0 +1,16 @@
+import com.sun.pojo.Books;
+import com.sun.service.BookService;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class myTest {
+    @Test
+    public void test(){
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        BookService bookServiceImpl = (BookService) context.getBean("BookServiceImpl");
+        for (Books books : bookServiceImpl.queryAllBook()) {
+            System.out.println(books);
+        }
+    }
+}
